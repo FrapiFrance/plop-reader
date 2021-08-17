244e1dbdf278a39f9f0588b3f824a25577c15fb1
fdilliers <fdilliers@efficity.com>
2021-08-17 09:53:03 +0200

    prevent log from beoing proposed as most recent book on the home and library

--- src/log.h
+++ src/log.h
@@ -30,7 +30,7 @@ public:
 
 private:
 	const char *DIR = FLASHDIR;
-	const char *FILEPATH = FLASHDIR "/plop-reader-logs.html";
+	const char *FILEPATH = FLASHDIR "/plop-reader-logs.log";
 
 	Log() {};
 	~Log() {};
